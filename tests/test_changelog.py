a0b504c44df6da36655436d0ce84de117706cd18
Felix Ji-13900K <feljii@outlook.com>
2026-07-31 21:54:38 +0800

    chore(release): honor manual bumps and sync changelog

--- tests/test_changelog.py
+++ tests/test_changelog.py
@@ -39,6 +39,7 @@ def test_changelog_uses_non_merge_commits_and_filters_merge_wording(
                 f"{'c3d4e5f' + '0' * 33}\x1f合并主分支",
                 f"{'d4e5f6a' + '0' * 33}\x1fchore: release v0.1.1",
                 f"{'d5e6f7a' + '0' * 33}\x1fchore(release): prepare 0.2.0",
+                f"{'d6e7f8a' + '0' * 33}\x1fchore(main): release 0.1.1",
                 f"{'e5f6a7b' + '0' * 33}\x1ffix: 修复导出",
             ]
         )
